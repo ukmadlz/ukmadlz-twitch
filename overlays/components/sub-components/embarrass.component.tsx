4aa910b37c2565becf848888c4573d4e0f842db1
Mike Elsmore <mike@elsmore.me>
2021-11-12 16:31:17 +0000

    An apology

--- overlays/components/sub-components/embarrass.component.tsx
+++ overlays/components/sub-components/embarrass.component.tsx
@@ -5,8 +5,11 @@ export default function EmbarrassComponent({ prompt }: any): JSX.Element {
     console.log('I am slightly ashamed')
     const clipID = prompt.split(' by ')[0];
     const parent = process.env.NEXT_PUBLIC_SITE_DOMAIN || process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost';
+    const iframeUrl = `https://clips.twitch.tv/embed?muted=false&autoplay=true&clip=${clipID}&parent=${parent}`;
+    console.log(iframeUrl)
     return ( 
         <div className={styles.embarrassedContainer}>
+            <p>Sorry if this doesn't load, twitch kinda sucks and OBS hurts to debug</p>
             <iframe
                 src={`https://clips.twitch.tv/embed?muted=false&autoplay=true&clip=${clipID}&parent=${parent}`}
                 frameBorder="0"
