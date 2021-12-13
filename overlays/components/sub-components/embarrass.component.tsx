d682b9165756d01755b9a81989ce21135178c3a4
Mike Elsmore <mike@elsmore.me>
2021-12-13 21:57:44 +0000

    Change to using the Cloudinary video player

--- overlays/components/sub-components/embarrass.component.tsx
+++ overlays/components/sub-components/embarrass.component.tsx
@@ -1,23 +1,21 @@
 import Fetch from 'node-fetch'
+import { Video } from 'cloudinary-react';
 import * as styles from './embarrass.css'
 
 export default function EmbarrassComponent({ prompt }: any): JSX.Element {
-    console.log('I am slightly ashamed')
     const clipID = prompt.split(' by ')[0];
     const parent = process.env.NEXT_PUBLIC_SITE_DOMAIN || process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost';
     const iframeUrl = `https://clips.twitch.tv/embed?muted=false&autoplay=true&clip=${clipID}&parent=${parent}`;
     console.log(iframeUrl)
     return ( 
         <div className={styles.embarrassedContainer}>
-            <p>Sorry if this doesn't load, twitch kinda sucks and OBS hurts to debug</p>
-            <iframe
-                src={`https://clips.twitch.tv/embed?muted=false&autoplay=true&clip=${clipID}&parent=${parent}`}
-                frameBorder="0"
-                allowFullScreen={true}
-                scrolling="no"
-                height="378"
-                width="620"
-            ></iframe>
+            <Video
+                width={620}
+                height={378}
+                autoPlay
+                cloudName="elsmore-me"
+                publicId={`twitch-overlay/clips/${clipID}`}
+            />
         </div>
         )
   }
