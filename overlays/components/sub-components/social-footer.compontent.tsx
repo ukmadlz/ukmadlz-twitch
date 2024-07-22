b91c090c8295903edeb6a101a0f8713ba9401396
Mike Elsmore <mike@elsmore.me>
2024-07-22 10:07:57 +0100

    Remove infobip from overlay

--- overlays/components/sub-components/social-footer.compontent.tsx
+++ overlays/components/sub-components/social-footer.compontent.tsx
@@ -1,48 +1,51 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import * as styles from './social-footer.css'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as styles from "./social-footer.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 
 library.add(fab, faCoffee);
 
-export default function SocialFooterComponent () {
-    return (
-        <div className={styles.socialContainer}>
-            <Carousel
-                autoPlay={true}
-                interval={10000}
-                infiniteLoop={true}
-                showArrows={false}
-                showStatus={false}
-                showIndicators={false}
-                showThumbs={false}
-                width={"20rem"}
-            >
-                <div>
-                    <FontAwesomeIcon className={styles.socialIcon} icon={['fab', 'github']} />/ukmadlz
-                </div>
-                <div>
-                    <FontAwesomeIcon className={styles.socialIcon} icon={['fab', 'twitter']} />/ukmadlz
-                </div>
-                <div>
-                    <FontAwesomeIcon className={styles.socialIcon} icon={['fab', 'twitch']} />/ukmadlz
-                </div>
-                <div>
-                    <span>elsmore.me</span>
-                </div>
-                <div>
-                    <img
-                        src="https://res.cloudinary.com/elsmore-me/image/upload/c_lpad,h_70,w_300/twitch-overlay/infobip_logo_white_RGB_1000x400px.png"
-                        style={{
-                            "width": "300px",
-                            "height": "70px",
-                        }}
-                    />
-                </div>
-            </Carousel>
+export default function SocialFooterComponent() {
+  return (
+    <div className={styles.socialContainer}>
+      <Carousel
+        autoPlay={true}
+        interval={10000}
+        infiniteLoop={true}
+        showArrows={false}
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        width={"20rem"}
+      >
+        <div>
+          <FontAwesomeIcon
+            className={styles.socialIcon}
+            icon={["fab", "github"]}
+          />
+          /ukmadlz
         </div>
-    );
+        <div>
+          <FontAwesomeIcon
+            className={styles.socialIcon}
+            icon={["fab", "twitter"]}
+          />
+          /ukmadlz
+        </div>
+        <div>
+          <FontAwesomeIcon
+            className={styles.socialIcon}
+            icon={["fab", "twitch"]}
+          />
+          /ukmadlz
+        </div>
+        <div>
+          <span>elsmore.me</span>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
